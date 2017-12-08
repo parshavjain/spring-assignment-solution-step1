90d3be6c4cf367a417ce8a4c235f9cf8fca7df5c
parsav.jain <parsav.jain@wipro.com>
2017-12-08 22:40:14 +0530

    update

--- src/main/java/com/stackroute/activitystream/model/Message.java
+++ src/main/java/com/stackroute/activitystream/model/Message.java
@@ -1,6 +1,7 @@
 package com.stackroute.activitystream.model;
 
 import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,22 +9,26 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/*
- * The class "Message" will be acting as the data model for the message Table in the database. Please
- * note that this class is annotated with @Entity annotation. Hibernate will scan all package for 
- * any Java objects annotated with the @Entity annotation. If it finds any, then it will begin the 
- * process of looking through that particular Java object to recreate it as a table in your database.
+/**
+ * The class "Message" will be acting as the data model for the message Table in
+ * the database. Please note that this class is annotated with @Entity
+ * annotation. Hibernate will scan all package for any Java objects annotated
+ * with the @Entity annotation. If it finds any, then it will begin the process
+ * of looking through that particular Java object to recreate it as a table in
+ * your database.
  */
 @Entity
 @Table(name = "MESSAGE")
 public class Message {
-	/*
+
+	/**
 	 * This class should have four fields (messageId,senderName,postedDate,message).
 	 * Out of these four fields, the field messageId should be auto-generated. This
 	 * class should also contain the getters and setters for the fields. The value
 	 * of postedDate should not be accepted from the user but should be always
 	 * initialized with the system date
 	 */
+
 	/**
 	 * Auto-Generated. Used to store message Id.
 	 */
@@ -31,16 +36,19 @@ public class Message {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "messageId", nullable = false)
 	int messageId;
+
 	/**
 	 * Used to store senderName.
 	 */
 	@Column(name = "senderName", nullable = false)
 	String sender;
+
 	/**
 	 * Used to store postedDate.
 	 */
 	@Column(name = "postedDate", nullable = false)
 	Date postedDate;
+
 	/**
 	 * Used to store message.
 	 */
@@ -58,7 +66,7 @@ public class Message {
 	 * @param messageId
 	 *            the messageId to set
 	 */
-	public void setMessageId(int messageId) {
+	public void setMessageId(final int messageId) {
 		this.messageId = messageId;
 	}
 
@@ -73,7 +81,7 @@ public class Message {
 	 * @param senderName
 	 *            the senderName to set
 	 */
-	public void setSenderName(String sender) {
+	public void setSenderName(final String sender) {
 		this.sender = sender;
 	}
 
@@ -88,7 +96,7 @@ public class Message {
 	 * @param senderName
 	 *            the senderName to set
 	 */
-	public void setSender(String sender) {
+	public void setSender(final String sender) {
 		setSenderName(sender);
 	}
 
@@ -120,32 +128,8 @@ public class Message {
 	 * @param message
 	 *            the message to set
 	 */
-	public void setMessage(String message) {
+	public void setMessage(final String message) {
 		this.message = message;
 	}
-	// public void setSenderName(String string) {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// public void setMessage(String string) {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// public void setPostedDate() {
-	// // TODO Auto-generated method stub
-	//
-	// }
-	//
-	// public String getSenderName() {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
-	//
-	// public String getMessage() {
-	// // TODO Auto-generated method stub
-	// return null;
-	// }
 
 }
